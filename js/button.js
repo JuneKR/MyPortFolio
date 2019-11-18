5fe92687b73f2dce50d9491ccec53d91f8b13909
Aekkaluk <jdei_nin@hotmail.com>
2019-11-19 00:44:27 +0700

    Switch Tab ไปหน้า Awards ได้โดยไม่ Refresh

--- js/button.js
+++ js/button.js
@@ -1,21 +1,30 @@
 const homeBTN = document.getElementById("homeBTN");
 const BTN = document.getElementById("myBTN");
-// var fac = document.getElementById("test");
-// document.getElementsByClassName("te").innerHTML = "test" + fac.innerHTML;
-BTN.addEventListener("click", alert("clicked"));
+
+
+// BTN.addEventListener("click", function(){
+//     document.getElementById('test1').style.visibility = "hidden";
+//     document.getElementById('test2').style.zIndex = 1;
+// });
+// homeBTN.addEventListener("click", function(){
+//     document.getElementById('test1').style.visibility = "visible";
+//     document.getElementById('test2').style.zIndex = -1;
+// })
+
+BTN.addEventListener("click", on);
 homeBTN.addEventListener("click", out);
             
 function on(){
     // document.getElementById("homeBTN").style.color = "white";
-    document.getElementById("test1").style.display = "none";
-    document.getElementById("test2").style.visibility = "visible";
+    document.getElementById('test1').style.visibility = "hidden";
+    document.getElementById('test2').style.zIndex = 1;
 }
 
 function out(){
     // document.getElementById("homeBTN").style.color = "blue";
     // document.getElementById("BTN").style.color = "white";
-    document.getElementById("test1").style.display = "block";
-    document.getElementById("test2").style.visibility = "hidden";
+    document.getElementById('test1').style.visibility = "visible";
+    document.getElementById('test2').style.zIndex = -1;
 }
 
-export {homeBTN, BTN};
+// alert('CONNECTED');
